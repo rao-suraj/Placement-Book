3e31970be82fc2051be71b021c975817fbdf2b70
Rao Suraj <surajrao250@gmail.com>
2023-05-21 10:57:53 +0530

    Adding Splash Screen

---
+++ app/src/main/java/com/example/placementbook/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.placementbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
